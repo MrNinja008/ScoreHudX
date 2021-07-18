7265f96a120c16daec5f6f50d20ee63fc55501aa
MrNinja008 <79526396+MrNinja008@users.noreply.github.com>
2021-07-18 09:55:33 +0530

    Update Main.php

--- BasicScore/src/Ifera/BasicScore/Main.php
+++ BasicScore/src/Ifera/BasicScore/Main.php
@@ -36,9 +36,7 @@ class Main extends PluginBase{
 			}
 
 			foreach($this->getServer()->getOnlinePlayers() as $player){
-				(new PlayerTagUpdateEvent($player, 
-                                 new ScoreTag("basicscore.ping", strval($player->getPing()))))->call();
-                                 new ScoreTag("basicscore.xp", strval($player->getXpLevel()))))->call();
+				(new PlayerTagUpdateEvent($player, new ScoreTag("basicscore.ping", strval($player->getPing()))))->call();, new ScoreTag("basicscore.xp", strval($player->getXpLevel()))))->call();
 			}
 
 			(new ServerTagsUpdateEvent([
